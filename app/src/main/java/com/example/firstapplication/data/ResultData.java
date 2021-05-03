e4dfdb4b8a5cbf0784c00390f24d542bbe290501
Ohzzi <clover7even@naver.com>
2021-05-03 17:07:26 +0900

    feat: 테스트 셋을 읽어오고 난 뒤 카운트 결과를 담는 클래스 생성

---
+++ app/src/main/java/com/example/firstapplication/data/ResultData.java
@@ -0,0 +1,38 @@
+package com.example.firstapplication.data;
+
+public class ResultData {
+
+    private final InputData minPvalData;
+    private final InputData maxPvalData;
+    private final int count0;
+    private final int count1;
+    private final int count2;
+
+    public ResultData(InputData minPvalData, InputData maxPvalData, int count0, int count1, int count2) {
+        this.minPvalData = minPvalData;
+        this.maxPvalData = maxPvalData;
+        this.count0 = count0;
+        this.count1 = count1;
+        this.count2 = count2;
+    }
+
+    public InputData getMinPvalData() {
+        return minPvalData;
+    }
+
+    public InputData getMaxPvalData() {
+        return maxPvalData;
+    }
+
+    public int getCount0() {
+        return count0;
+    }
+
+    public int getCount1() {
+        return count1;
+    }
+
+    public int getCount2() {
+        return count2;
+    }
+}
